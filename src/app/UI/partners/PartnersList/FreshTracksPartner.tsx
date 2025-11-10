9a69e533e78d33190771a8d409c3b7e383eded0e
evgenydus <evgeny21dus@gmail.com>
2025-11-10 22:08:08 +0400

    fix: images params

--- src/app/UI/partners/PartnersList/FreshTracksPartner.tsx
+++ src/app/UI/partners/PartnersList/FreshTracksPartner.tsx
@@ -12,7 +12,7 @@ const FreshTracksPartner = ({ partner }: { partner: Partner }) => {
     <Drawer content={<PartnerInfo partner={partner} />} title={name}>
       <button className="flex items-center gap-2 rounded-xl bg-gray-100 p-3" type="button">
         <div className="flex h-24 flex-none items-center justify-center overflow-hidden rounded-xl">
-          <Image alt={`${name} logo`} src={logo} width={96} />
+          <Image alt={`${name} logo`} className="object-contain" src={logo} width={96} />
         </div>
 
         <h4 className=" text-lg font-semibold text-gray-900">{name}</h4>
