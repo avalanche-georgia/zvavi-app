4b0a6b62767501798b8b850d657a80c05f052858
Andrew Posohov <62482805+Greemrock@users.noreply.github.com>
2025-11-17 15:46:14 +0300

    feat: [Admin] reaction on "Reset password" (#63)

--- src/app/[locale]/auth/forgot-password/page.tsx
+++ src/app/[locale]/auth/forgot-password/page.tsx
@@ -4,7 +4,9 @@ import { useCallback, useState } from 'react'
 import { Field, Fieldset, Label, Legend } from '@headlessui/react'
 import { useLocale, useTranslations } from 'next-intl'
 
+import { handleSupabaseError } from '@/data/helpers'
 import { routes } from '@/UI/header/NavMenu/constants'
+import { useToast } from '@/UI/hooks'
 
 import { Button, TextInput } from '@/UI/components/inputs'
 
@@ -12,53 +14,59 @@ import { supabase } from '@/data'
 
 const ForgotPasswordPage = () => {
   const t = useTranslations()
+  const { toastError, toastSuccess } = useToast()
   const locale = useLocale()
 
   const [email, setEmail] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
+  const [isPending, setIsPending] = useState(false)
 
   const handleEmailChange = useCallback(
     ({ target }: React.ChangeEvent<HTMLInputElement>) => setEmail(target.value),
-    [setEmail],
+    [],
   )
 
   const handleSendResetLinkClick = useCallback(
     async (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault()
+      setIsPending(true)
 
-      const { error } = await supabase.auth.resetPasswordForEmail(email, {
-        // TODO: Check if window.location.origin works on production
-        redirectTo: `${window.location.origin}/${locale}${routes.setNewPassword}`,
-      })
+      try {
+        const { error } = await supabase.auth.resetPasswordForEmail(email, {
+          redirectTo: `${window.location.origin}/${locale}${routes.setNewPassword}`,
+        })
 
-      if (error) {
-        setErrorMessage(error.message)
-      } else {
-        setErrorMessage('')
+        handleSupabaseError(error)
+        toastSuccess(t('auth.forgotPassword.messages.sentSuccessfully'))
+      } catch (error) {
+        toastError('ForgotPasswordPage | handleSendResetLinkClick', {
+          error,
+        })
+      } finally {
+        setIsPending(false)
       }
     },
-    [email, locale],
+    [email, locale, t, toastError, toastSuccess],
   )
 
   return (
-    <form className="w-full max-w-lg px-4" onSubmit={handleSendResetLinkClick}>
-      <Fieldset className="space-y-6 rounded p-6 dark:text-white sm:p-10">
-        <Legend className="text-center text-2xl font-semibold">
-          {t('auth.forgotPassword.title')}
-        </Legend>
+    <div className="flex w-full flex-col items-center justify-center">
+      <form className="w-full max-w-sm px-4" onSubmit={handleSendResetLinkClick}>
+        <Fieldset className="space-y-6 rounded p-6 dark:text-white sm:p-10">
+          <Legend className="text-center text-2xl font-semibold">
+            {t('auth.forgotPassword.title')}
+          </Legend>
 
-        <Field>
-          <Label className="text-sm/6">{t('common.labels.email')}</Label>
-          <TextInput onChange={handleEmailChange} required type="email" />
-        </Field>
+          <Field>
+            <Label className="text-sm/6">{t('common.labels.email')}</Label>
+            <TextInput className="w-full" onChange={handleEmailChange} required type="email" />
+          </Field>
 
-        <Button className="ml-auto" type="submit">
-          {t('auth.forgotPassword.sendResetLink')}
-        </Button>
-      </Fieldset>
-
-      {errorMessage && <p className="mt-4 text-center text-red-500">{errorMessage}</p>}
-    </form>
+          <Button className="w-full text-center" disabled={isPending} type="submit">
+            <span className="w-full">{t('auth.forgotPassword.sendResetLink')}</span>
+          </Button>
+        </Fieldset>
+      </form>
+    </div>
   )
 }
 
