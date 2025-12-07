b2012f19ce9a3091768ca7749fef6b68d93bb358
evgenydus <evgeny21dus@gmail.com>
2025-12-07 20:21:25 +0400

    feat: enhase badge ui

--- src/app/UI/partners/PartnersList/PartnerBadge.tsx
+++ src/app/UI/partners/PartnersList/PartnerBadge.tsx
@@ -1,26 +1,37 @@
+import clsx from 'clsx'
+
 import type { Partner } from '@/data/constants/partners'
+import { partners } from '@/data/constants/partners'
 
 import { Drawer } from '@/UI/components'
 import PartnerInfo from './PartnerInfo'
 import PartnerLogo from './PartnerLogo'
 
-const badgeClassName =
-  'flex size-24 flex-none items-center justify-center rounded-2xl bg-gray-100 p-2'
-
 const PartnerBadge = ({ partner }: { partner: Partner }) => {
   const { infoKey, isRounded, logo, name, url } = partner
   const hasInfo = Boolean(infoKey)
 
+  const isTopTier = partners[1].some((topPartner) => topPartner.id === partner.id)
+
+  const badgeClassName = clsx(
+    'flex size-24 flex-none items-center justify-center rounded-2xl p-2',
+    isTopTier ? 'bg-primary/10' : 'bg-gray-100',
+  )
+
+  const logoNode = (
+    <PartnerLogo
+      className="size-full"
+      imageSize={{ height: 80, width: 80 }}
+      isRounded={isRounded}
+      logo={logo}
+      name={name}
+    />
+  )
+
   if (!hasInfo) {
     return (
       <a className={badgeClassName} href={url} rel="noopener noreferrer" target="_blank">
-        <PartnerLogo
-          className="size-full"
-          imageSize={{ height: 80, width: 80 }}
-          isRounded={isRounded}
-          logo={logo}
-          name={name}
-        />
+        {logoNode}
       </a>
     )
   }
@@ -28,13 +39,7 @@ const PartnerBadge = ({ partner }: { partner: Partner }) => {
   return (
     <Drawer content={<PartnerInfo partner={partner} />} title={name}>
       <button className={badgeClassName} type="button">
-        <PartnerLogo
-          className="size-full"
-          imageSize={{ height: 80, width: 80 }}
-          isRounded={isRounded}
-          logo={logo}
-          name={name}
-        />
+        {logoNode}
       </button>
     </Drawer>
   )
