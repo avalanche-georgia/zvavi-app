7b6298e9b2b07cf4e32e6851075bc603b3c9b69e
Evgeny Dus <evgeny21dus@gmail.com>
2025-12-21 21:14:03 +0400

    feat: implement not found pages (#88)

---
+++ src/app/[locale]/forecasts/[id]/not-found.tsx
@@ -0,0 +1,21 @@
+import { PageWrapper } from '@components/layout'
+import { getTranslations } from 'next-intl/server'
+import { ButtonLink } from 'src/components/shared'
+
+import { routes } from '@/routes'
+
+const ForecastNotFound = async () => {
+  const t = await getTranslations()
+
+  return (
+    <PageWrapper>
+      <div className="flex flex-col items-center justify-center gap-6 py-20 text-center">
+        <h1 className="text-2xl font-semibold text-gray-900">{t('forecast.notFound.title')}</h1>
+        <p className="max-w-md text-gray-600">{t('forecast.notFound.message')}</p>
+        <ButtonLink href={routes.currentForecast}>{t('forecast.notFound.linkText')}</ButtonLink>
+      </div>
+    </PageWrapper>
+  )
+}
+
+export default ForecastNotFound
---
+++ src/app/not-found.tsx
@@ -0,0 +1,36 @@
+import avalancheGIF from '@assets/images/avalanche.gif'
+import { PageWrapper } from '@components/layout'
+import { ButtonLink } from '@components/shared'
+import Image from 'next/image'
+import { getTranslations } from 'next-intl/server'
+
+import { routes } from '@/routes'
+
+const NotFound = async () => {
+  const t = await getTranslations()
+
+  return (
+    <PageWrapper>
+      <div className="flex max-w-full flex-col items-center justify-center gap-6 py-6 text-center lg:py-20">
+        <div className="relative">
+          <p className="absolute size-full pt-2 text-7xl opacity-15 lg:pt-3 lg:text-8xl">404</p>
+
+          <Image
+            alt="404 Not Found"
+            className="rounded-lg"
+            height={270}
+            src={avalancheGIF}
+            width={480}
+          />
+        </div>
+
+        <h1 className="text-2xl font-semibold text-gray-900">{t('common.notFound.title')}</h1>
+        <p className="max-w-lg whitespace-pre-line text-gray-600">{t('common.notFound.message')}</p>
+
+        <ButtonLink href={routes.currentForecast}>{t('common.notFound.linkText')}</ButtonLink>
+      </div>
+    </PageWrapper>
+  )
+}
+
+export default NotFound
