b78b254a631dfe4009832c05bfc1f2342a519297
evgenydus <evgeny21dus@gmail.com>
2026-02-13 22:19:26 +0400

    feat: implement admin notifications

--- src/components/features/members/MemberApplicationForm/submitApplication.ts
+++ src/components/features/members/MemberApplicationForm/submitApplication.ts
@@ -4,6 +4,14 @@ import type { MemberApplicationFormData } from './schema'
 
 export type SubmitResult = { memberId: string; verificationCode: string }
 
+const notifyAdmin = (name: string, email: string, paymentMethod: string) => {
+  fetch('/api/notify/new-application', {
+    body: JSON.stringify({ email, name, paymentMethod }),
+    headers: { 'Content-Type': 'application/json' },
+    method: 'POST',
+  }).catch(() => {})
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const submitApplication = async ({ charterAgreed, ...data }: MemberApplicationFormData) => {
   const { address, age, gender, motivation, occupation, ...rest } = data
@@ -22,6 +30,8 @@ const submitApplication = async ({ charterAgreed, ...data }: MemberApplicationFo
   })
 
   if (error) throw error
+
+  notifyAdmin(`${rest.firstName} ${rest.lastName}`, rest.email, rest.paymentMethod)
 
   return result as SubmitResult
 }
